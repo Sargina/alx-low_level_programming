a9b985f64d8ddcb12d4144c162d76372906a624e
Sargina <sarginasuzan@gmail.com>
2022-03-21 19:09:00 +0300

    my first commit

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef_main_h
+#define _main_h
+
+void reset_to_98(int *n)[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[B[D[D[D[D _main_h[B[B[B[C[C[C[C[C[C[C[C[C;
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif
