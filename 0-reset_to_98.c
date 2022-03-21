1ea42424ca49e78fcd5c8a4a9295b2b51f3e260c
Sargina <sarginasuzan@gmail.com>
2022-03-21 19:21:27 +0300

    0-reset_to_98.c

---
+++ 0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+
+ * reset_to_98 - function that takes a pointer to an int as parameter 
+ *
+[A[C[C and updates the value it points to to 98.
+ *
+ * @n; number.
+ *
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
